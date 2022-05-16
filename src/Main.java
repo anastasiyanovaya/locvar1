62a3d46ad1e634348b2b9e97df9f49863dfd3382
anastasiynovaya <aefremova3014@gmail.com>
2022-05-16 13:21:47 +0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 210;
+        int bonus = 20;
+        int mile = ticketPrice / bonus;
+        System.out.println(mile);
+
+    }
+}
